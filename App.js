f71c6401f9db73b2ea1c71e05461c214ee8290ae
Juan Cabona <jdcabona2@gmail.com>
2022-05-18 17:22:04 -0300

    instalacion de dependecias

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { sequelize } = require('sequelize');
 
 app.listen(3000, () => console.log(" Servidor levantado en el puerto 3000 "));
 
@@ -11,3 +12,9 @@ app.get('/api', (req, res) => {
     res.send("Aca responde la api");
 });
 
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
